97f4b81d01b8d11641825857be336a86b5291cd4
Atlanta Sports <atlantasportsloja@gmail.com>
2026-05-02 03:39:38 -0300

    fix: forcar lookup IPv4 no nodemailer

--- backend/src/lib/mailer.js
+++ backend/src/lib/mailer.js
@@ -1,10 +1,13 @@
 const nodemailer = require('nodemailer');
+const dns = require('dns');
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 465,
   secure: true,
-  family: 4,
+  lookup: (hostname, options, callback) => {
+    dns.lookup(hostname, { ...options, family: 4 }, callback);
+  },
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
